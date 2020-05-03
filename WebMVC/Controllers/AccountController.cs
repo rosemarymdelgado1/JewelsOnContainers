b9f51bc14cb23e475d8dd843da827726838a7df7
Rosemary Delgado <33888379+rosemarymdelgado1@users.noreply.github.com>
2020-05-03 00:23:29 -0700

    Fixes...

--- WebMVC/Controllers/AccountController.cs
+++ WebMVC/Controllers/AccountController.cs
@@ -1,21 +1,20 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
-using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
-using WebMVC.Controllers;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
-namespace WebMvc.Controllers
+namespace WebMVC.Controllers
 {
     [Authorize]
     public class AccountController : Controller
     {
-
-
         [Authorize]
         public async Task<IActionResult> SignIn(string returnUrl)
         {
@@ -43,19 +42,6 @@ namespace WebMvc.Controllers
             return RedirectToAction(nameof(CatalogController.About), "Catalog");
         }
 
-        //public async Task<IActionResult> Signout()
-        //{
-        //    await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-        //    await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme);
-
-        //    ////// "Catalog" because UrlHelper doesn't support nameof() for controllers
-        //    ////// https://github.com/aspnet/Mvc/issues/5853
-        //    var homeUrl = Url.Action(nameof(CatalogController.Index), "Catalog");
-        //    return new SignOutResult(OpenIdConnectDefaults.AuthenticationScheme,
-        //        new AuthenticationProperties { RedirectUri = homeUrl });
-        //}
-
-
         public async Task<IActionResult> Signout()
         {
             await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
@@ -67,8 +53,5 @@ namespace WebMvc.Controllers
             return new SignOutResult(OpenIdConnectDefaults.AuthenticationScheme,
                 new AuthenticationProperties { RedirectUri = homeUrl });
         }
-
     }
 }
-
- 
--- WebMVC/Startup.cs
+++ WebMVC/Startup.cs
@@ -2,18 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.IdentityModel.Tokens;
 using WebMVC.Infrastructure;
 using WebMVC.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
 
 namespace WebMVC
 {
@@ -72,21 +72,23 @@ namespace WebMVC
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+            //if (env.IsDevelopment())
+            //{
+            app.UseDeveloperExceptionPage();
+            //}
+            //else
+            //{
+            //    app.UseExceptionHandler("/Home/Error");
+            //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            //    app.UseHsts();
+            //}
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
 
+            app.UseAuthentication();
+            app.UseCookiePolicy();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
